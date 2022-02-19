d61603211ece9815e3a00d426f34104a4215bb10
Axl Orellana <aorellana@smartdream.es>
2022-02-18 20:50:21 -0500

    initial commit

---
+++ example/swapi_repository_example.dart
@@ -0,0 +1,6 @@
+import 'package:swapi_repository/swapi_repository.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/swapi_repository.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library swapi_repository;
+
+export 'src/swapi_repository_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
