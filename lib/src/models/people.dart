ff0fc1e71a9886f78c2a0c06b5e338e866620a22
Axl Orellana <aorellana@smartdream.es>
2022-02-19 00:32:02 -0500

    add people model

---
+++ lib/src/models/people.dart
@@ -0,0 +1,56 @@
+class People {
+  const People({
+    required this.name,
+    required this.height,
+    required this.mass,
+    required this.hairColor,
+    required this.skinColor,
+    required this.eyeColor,
+    required this.birthYear,
+    required this.gender,
+    required this.homeworld,
+    required this.films,
+    required this.species,
+    required this.vehicles,
+    required this.starships,
+    required this.created,
+    required this.edited,
+    required this.url,
+  });
+
+  factory People.fromJson(Map<String, dynamic> json) => People(
+        name: json['name'] as String?,
+        height: json['height'] as String?,
+        mass: json['mass'] as String?,
+        hairColor: json['hair_color'] as String?,
+        skinColor: json['skin_color'] as String?,
+        eyeColor: json['eye_color'] as String?,
+        birthYear: json['birth_year'] as String?,
+        gender: json['gender'] as String?,
+        homeworld: json['homeworld'] as String?,
+        films: (json['films'] as List<String>?)?.cast<String>(),
+        species: (json['species'] as List<String>?)?.cast<String>(),
+        vehicles: (json['vehicles'] as List<String>?)?.cast<String>(),
+        starships: (json['starships'] as List<String>?)?.cast<String>(),
+        created: json['created'] as String?,
+        edited: json['edited'] as String?,
+        url: json['url'] as String?,
+      );
+
+  final String? name;
+  final String? height;
+  final String? mass;
+  final String? hairColor;
+  final String? skinColor;
+  final String? eyeColor;
+  final String? birthYear;
+  final String? gender;
+  final String? homeworld;
+  final List<String>? films;
+  final List<String>? species;
+  final List<String>? vehicles;
+  final List<String>? starships;
+  final String? created;
+  final String? edited;
+  final String? url;
+}
