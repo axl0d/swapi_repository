84a24c1b390e66d9431260619333e40eb2c688b4
Axl Orellana <aorellana@smartdream.es>
2022-02-24 09:50:17 -0500

    export model

--- lib/swapi_repository.dart
+++ lib/swapi_repository.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library swapi_repository;
 
+export 'src/models/people.dart';
 export 'src/swapi_repository_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
