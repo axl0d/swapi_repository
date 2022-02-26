c9d3a5bc0554bfbd5abb799abd44e6c3b95ab894
Axl Orellana <aorellana@smartdream.es>
2022-02-25 21:11:03 -0500

    add multiple fetch

--- lib/src/dio_client.dart
+++ lib/src/dio_client.dart
@@ -28,4 +28,21 @@ class DioClient {
       throw ServerFailure();
     }
   }
+
+  Future<List<Response<Map<String, dynamic>?>>> multipleFetch(
+    List<String> uris, {
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    try {
+      final response = await Future.wait(
+        uris.map((u) => _dio.get<Map<String, dynamic>?>(u)).toList(),
+      );
+      return response;
+    } on DioError catch (e) {
+      if (e.response!.statusCode! >= 400 && e.response!.statusCode! < 500) {
+        throw ClientFailure();
+      }
+      throw ServerFailure();
+    }
+  }
 }
